369626f65162d9a0f4d4038c2419fda636a5ac1b
Andrzej Jozwik <ajozwik@dotsystems.pl>
2019-08-19 14:54:37 +0200

    Wrong type

--- quill-monix-macro/src/main/scala/pl/jozwik/quillgeneric/quillmacro/monix/MonixRepository.scala
+++ quill-monix-macro/src/main/scala/pl/jozwik/quillgeneric/quillmacro/monix/MonixRepository.scala
@@ -7,17 +7,17 @@ trait MonixRepository[K, T <: WithId[K]] extends Repository[K, T] with WithMonix
 trait MonixRepositoryWithGeneratedId[K, T <: WithId[K]] extends RepositoryWithGeneratedId[K, T] with WithMonix
 
 trait MonixRepositoryLong[K, T <: WithId[K]] extends Repository[K, T] with WithMonix {
-  type A = Long
+  type U = Long
 }
 
 trait MonixRepositoryWithGeneratedIdLong[K, T <: WithId[K]] extends RepositoryWithGeneratedId[K, T] with WithMonix {
-  type A = Long
+  type U = Long
 }
 
 trait MonixRepositoryUnit[K, T <: WithId[K]] extends Repository[K, T] with WithMonix {
-  type A = Unit
+  type U = Unit
 }
 
 trait MonixRepositoryWithGeneratedIdUnit[K, T <: WithId[K]] extends RepositoryWithGeneratedId[K, T] with WithMonix {
-  type A = Unit
+  type U = Unit
 }
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := "0.7.1.4-SNAPSHOT"
+ThisBuild / version := "0.7.1.4"
