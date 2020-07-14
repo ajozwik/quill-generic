88f298082a3a4cfae552de1b96b46db98b3f4d53
Andrzej Jozwik <ajozwik@dotsystems.pl>
2020-07-14 17:04:07 +0200

    Solved #17

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PATH=$HOME/bin:$PATH sbt -Dquill.macro.log=false clean test publishLocal publishSigned sonatypeRelease
+PATH=$HOME/bin:$PATH sbt -Dquill.macro.log=false clean +test +publishLocal publishSigned sonatypeRelease
--- publish213.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PATH=$HOME/bin:$PATH sbt -Dquill.macro.log=false -Dmacro.scala.version=2.13 clean test publishLocal publishSigned sonatypeRelease
