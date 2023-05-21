b54cdba891631e06858d6283b1fb26ccbd033e55
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2023-05-21 19:46:29 +0200

    simplify code

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . $HOME/.sbt/.quill-generic-coverage
-sbt8 -Dquill.macro.log=false clean +test +coverageAggregate && sbt8 +coverageAggregate || exit 1
-PATH=$HOME/bin:$PATH sbt8 -Dquill.macro.log=false +publishLocalSigned +publishSigned +sonatypeRelease
+sbt8 -Dquill.macro.log=false clean +coverage +test +coverageAggregate && sbt8 +coveralls || exit 1
+PATH=$HOME/bin:$PATH sbt8 -Dquill.macro.log=false clean +publishLocalSigned +publishSigned +sonatypeRelease
