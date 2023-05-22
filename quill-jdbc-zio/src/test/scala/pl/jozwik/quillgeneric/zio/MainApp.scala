378246c197fcb4266481e21720e74a52439645aa
Andrzej Jozwik <ajozwik@dotsystems.pl>
2023-05-22 12:53:40 +0200

    catch errors by mapErrorCause

--- quill-jdbc-zio/src/test/scala/pl/jozwik/quillgeneric/zio/MainApp.scala
+++ quill-jdbc-zio/src/test/scala/pl/jozwik/quillgeneric/zio/MainApp.scala
@@ -7,7 +7,6 @@ import pl.jozwik.quillgeneric.Strategy
 import pl.jozwik.quillgeneric.model.{ Configuration, ConfigurationId }
 import pl.jozwik.quillgeneric.repository.DateQuotes
 import pl.jozwik.quillgeneric.zio.repository.ConfigurationJdbcRepository
-import zio.Console.printLine
 import zio.ZIOAppDefault
 import zio.interop.catz.*
 object MainApp extends ZIOAppDefault with StrictLogging {
@@ -15,21 +14,44 @@ object MainApp extends ZIOAppDefault with StrictLogging {
   private val strategy        = Strategy.namingStrategy
   private lazy val ctx        = new H2ZioJdbcContext(strategy) with ObjectGenericTimeDecoders with ObjectGenericTimeEncoders with DateQuotes
   private lazy val repository = new ConfigurationJdbcRepository(ctx)
+  private val id              = ConfigurationId("key")
 
-  private val app = for {
-    _ <- repository.create(Configuration(ConfigurationId("key"), "value"))
-    a <- repository.all
+  private val appSuccess = for {
+    _  <- repository.create(Configuration(id, "value"))
+    a  <- repository.all
+    el <- repository.readUnsafe(id)
+    _  <- repository.deleteAll
+  } yield {
+    el
+  }
+
+  private val appFail = for {
+    _  <- repository.create(Configuration(ConfigurationId("key"), "value"))
+    a  <- repository.all
+    el <- repository.readUnsafe(ConfigurationId("aaa"))
+
   } yield {
     a
   }
 
   def run = {
-    logger.debug("Start")
-    for {
-      s <- app.tap(result => printLine(result.toString)).provideEnvironment(ZioHelperSpec.environment).exitCode
+    val suc = for {
+      success <- appSuccess.provideEnvironment(ZioHelperSpec.environment)
     } yield {
-      logger.debug(s"Stop $s")
+      logger.debug(s"Success $success ")
     }
 
+    for {
+      _ <- suc
+      c <- appFail
+        .provideEnvironment(ZioHelperSpec.environment)
+        .mapErrorCause(e => {
+          logger.error(s"$e")
+          e
+        })
+        .exitCode
+    } yield {
+      logger.debug(s"$c")
+    }
   }
 }
