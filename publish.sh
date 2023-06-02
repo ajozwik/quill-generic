91f0dc9c0107b111bd070405ea40f4ae8b791f4d
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2023-06-02 22:42:53 +0200

    1.9.0

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-. $HOME/.sbt/.quill-generic-coverage
-sbt8 -Dquill.macro.log=false clean +coverage +test +coverageAggregate && sbt8 +coveralls || exit 1
 PATH=$HOME/bin:$PATH sbt8 -Dquill.macro.log=false clean +publishLocalSigned +publishSigned +sonatypeRelease
